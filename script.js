ce34a0c0fbada8081cdfadb51d38b29c47d3e42d
Matheus Chaves de Farias <matheusc.farias123@gmail.com>
2022-08-02 18:32:37 -0300

    Desafio 6

---
+++ script.js
@@ -0,0 +1,9 @@
+//preciso pegar um elemento e atribuir a ele a class 'selected'
+
+function colorSelected() {
+  let color = document.querySelector('.color');
+  
+  color.classList.add('selected');
+
+}
+colorSelected();
