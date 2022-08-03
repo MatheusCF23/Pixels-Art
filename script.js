ec1a1b600ac8ed4bbdb3e6e9b0b5b17bf0bc612c
Matheus Chaves de Farias <matheusc.farias123@gmail.com>
2022-08-02 22:44:47 -0300

    Desafio 8

--- script.js
+++ script.js
@@ -2,10 +2,9 @@
 
 function colorSelected() {
   let color = document.querySelector('.color');
-  
   color.classList.add('selected');
-
 }
+
 colorSelected();
 
 
@@ -13,9 +12,36 @@ colorSelected();
 
 let x = document.getElementById('color-palette');
 function mudarCor (parametro) {
-  let selected = document.querySelector('.selected')
-  selected.classList.remove('selected')
-  parametro.target.classList.add('selected')
+  let selected = document.querySelector('.selected');
+  selected.classList.remove('selected');
+  parametro.target.classList.add('selected');
 }
 
-x.addEventListener('click', mudarCor)
+x.addEventListener('click', mudarCor);
+
+// Desafio 8
+
+let t = document.getElementById('color-palette');
+let y = document.getElementById('pixel-board');
+let a = 'rgb(0, 0, 0)';
+
+function resultado () {
+  let preto = document.getElementsByClassName('color')[0];
+  preto.classList.add('selected');
+}
+resultado();
+
+function colorTrocar (parametro2) {
+  let selected = document.querySelector('.selected');
+  selected.classList.remove('selected');
+  parametro2.target.classList.add('selected');
+  let cor = window.getComputedStyle(selected);
+  let corAdd = cor.getPropertyValue('background-color');
+  return a = corAdd;
+}
+t.addEventListener('click', colorTrocar);
+
+function addCor (parametro3) {
+  parametro3.target.style.backgroundColor = a
+}
+y.addEventListener('click', addCor);
