4ecf6a41a7c34ce1df682dbbbda05ef935e856ad
Matheus Chaves de Farias <matheusc.farias123@gmail.com>
2022-08-08 10:40:23 -0300

    Desafio 12

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+function criaTabela () {
+  let paiTabela = document.getElementById('pixel-board');
+  for(let i = 1; i <= 25; i += 1) {
+    let div = document.createElement('div');
+    div.className = 'pixel';
+    paiTabela.appendChild(div);
+  }
+}
+criaTabela();
+
+
 //preciso pegar um elemento e atribuir a ele a class 'selected'
+// Desafio 6
 
 function colorSelected() {
   let color = document.querySelector('.color');
@@ -11,7 +23,7 @@ colorSelected();
 //Desafio 7
 
 let x = document.getElementById('color-palette');
-function mudarCor (parametro) {
+function mudarCor(parametro) {
   let selected = document.querySelector('.selected');
   selected.classList.remove('selected');
   parametro.target.classList.add('selected');
@@ -25,13 +37,13 @@ let t = document.getElementById('color-palette');
 let y = document.getElementById('pixel-board');
 let a = 'rgb(0, 0, 0)';
 
-function resultado () {
+function resultado() {
   let preto = document.getElementsByClassName('color')[0];
   preto.classList.add('selected');
 }
 resultado();
 
-function colorTrocar (parametro2) {
+function colorTrocar(parametro2) {
   let selected = document.querySelector('.selected');
   selected.classList.remove('selected');
   parametro2.target.classList.add('selected');
@@ -41,7 +53,7 @@ function colorTrocar (parametro2) {
 }
 t.addEventListener('click', colorTrocar);
 
-function addCor (parametro3) {
+function addCor(parametro3) {
   parametro3.target.style.backgroundColor = a
 }
 y.addEventListener('click', addCor);
@@ -50,10 +62,49 @@ y.addEventListener('click', addCor);
 // Desafio 9
 let z = document.getElementById('clear-board');
 
-function limpar () {
-   let selected = document.getElementsByClassName('pixel');
-   for (let i = 0; i < selected.length; i += 1){
+function limpar() {
+  let selected = document.getElementsByClassName('pixel');
+  for (let i = 0; i < selected.length; i += 1) {
     selected[i].style.backgroundColor = 'white';
-   }
-   }
+  }
+}
 z.addEventListener('click', limpar);
+
+
+// Desafio 10
+
+//const boardSize = document.getElementById('board-size');
+
+
+
+
+
+
+
+
+
+
+
+
+//Desafio 12
+
+function createPaletteColors() {
+  const colors = ['black'];
+  const spanColor = document.querySelectorAll('.color');
+  const lettersNumber = '0123456789ABCDEF';
+  let color = '#';
+  
+  for (let i = 0; i < 3; i += 1) {
+    color = '#';
+    for (let index = 0; index < 6; index += 1) {
+      color += lettersNumber[Math.floor(Math.random() * 16)];
+    }
+    colors.push(color);
+  }
+
+  for (let i = 0; i < spanColor.length; i += 1) {
+    spanColor[i].style.backgroundColor = colors[i];
+  }
+  spanColor[0].classList.add('selected');
+}
+createPaletteColors();
